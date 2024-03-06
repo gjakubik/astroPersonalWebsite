39da9c15a0a00a4a07c2dde968bc418cc542dfcc
Gavin Jakubik <gavito8800@gmail.com>
2024-03-06 16:12:33 -0500

    Initial Commit

---
+++ src/components/ReactComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function ReactComponent() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      This is a React component styled with Tailwind CSS!
+    </div>
+  );
+}
